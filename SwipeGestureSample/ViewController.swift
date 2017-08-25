857a71e02620191c4790300b81030cf1dd05c2ef
이명석 <leams0814@gmail.com>
2017-08-26 08:31:42 +0900

    Initial Commit

---
+++ SwipeGestureSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwipeGestureSample
+//
+//  Created by Lee Myung seok on 2017. 8. 26..
+//  Copyright © 2017년 Lee Myung seok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
